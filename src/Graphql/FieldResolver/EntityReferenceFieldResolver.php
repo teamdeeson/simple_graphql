6adaa16351b6f05506f3d6d21ebacd20a83a0b46
Peter Berryman <peterb@deeson.co.uk>
2022-03-22 11:31:31 +0000

    Entity Reference Field Resolver

--- src/Graphql/FieldResolver/EntityReferenceFieldResolver.php
+++ src/Graphql/FieldResolver/EntityReferenceFieldResolver.php
@@ -6,13 +6,33 @@ use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\TypedData\TypedDataInterface;
 use GraphQL\Type\Definition\ResolveInfo;
 
-class EntityReferenceFieldResolver extends FieldResolverBase {
+class EntityReferenceFieldResolver extends DrupalEntityFieldResolver {
   public function applies(ContentEntityInterface $entity, $args, $context, ResolveInfo $info) {
     return $entity->hasField($info->fieldName) &&
       $entity->getFieldDefinition($info->fieldName)->getType() === "entity_reference";
   }
 
+  public function resolve(ContentEntityInterface $entity, $args, $context, ResolveInfo $info) {
+    if ($info->fieldName === "id") {
+      return $entity->uuid();
+    }
+
+    if ($info->fieldName === "path") {
+      return $entity->toUrl()->toString();
+    }
+
+   
+
+    return parent::resolve($entity, $args, $context, $info);
+  }
+
   public function resolveRow(TypedDataInterface $item, $definition, $args, $context, ResolveInfo $info) {
-    return $item->entity;
+    $myFields = [];
+    foreach ($item->entity->getFields() as $name => $field) {
+      $myFields[$name] = $field->getValue();
+    }
+    $myFields['id'] = $item->entity->id();
+    $myFields['title'] = $item->entity->getTitle();
+    return $myFields;
   }
 }
